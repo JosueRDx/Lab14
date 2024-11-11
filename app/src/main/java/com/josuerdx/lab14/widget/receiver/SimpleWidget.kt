3494423862a96e18123206aef7956e5ed9bc8b61
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-11-11 13:14:44 -0500

    feat: Importar SimpleWidgetContent configurar widget

--- app/src/main/java/com/josuerdx/lab14/widget/receiver/SimpleWidget.kt
+++ app/src/main/java/com/josuerdx/lab14/widget/receiver/SimpleWidget.kt
@@ -2,6 +2,7 @@ package com.josuerdx.lab14.widget.receiver
 
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import com.josuerdx.lab14.widget.content.SimpleWidgetContent
 
 class SimpleWidget : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
