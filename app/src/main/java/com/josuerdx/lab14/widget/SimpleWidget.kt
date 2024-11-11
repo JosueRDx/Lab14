eb29ed539d3aad805df94d05c755334dd3ed653f
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-11-11 12:42:57 -0500

    feat: Crear clase SimpleWidget para GlanceAppWidget

---
+++ app/src/main/java/com/josuerdx/lab14/widget/SimpleWidget.kt
@@ -0,0 +1,8 @@
+package com.josuerdx.lab14.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
+}
